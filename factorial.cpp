debb4ce48aa80c12c8b8b1b6825e452221cafb4d
Mir Mohammod Tahasin <tahsinahmed.iit@gmail.com>
2024-04-25 12:22:06 +0600

    This is the extension of fibonacci series from experimental-branch.

--- factorial.cpp
+++ factorial.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,15 +8,37 @@ int factorial(int n) {
         return n * factorial(n - 1);
 }
 
+void fibonacciSeries(int n) {
+    int first = 0, second = 1, next;
+    cout << "Fibonacci sequence up to " << n << " terms: ";
+
+    for (int i = 0; i < n; ++i) {
+        if (i <= 1)
+            next = i;
+        else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        cout << next << " ";
+    }
+}
+
 int main() {
     int number;
     //This if from main branch
     cout << "Enter a number: ";
     cin >> number;
     cout << "Factorial of " << number << " is " << factorial(number) << endl;
+
+    //For Fibonacci Series
+    cout<<"Enter the number for fibonacci series"<<endl;
+    int n;
+    cin>>n;
+    fibonacciSeries(n)
+
     return 0;
 }
-=======
 
 
 
@@ -31,4 +52,3 @@ int main() {
 
 
 //this is from feature branch
->>>>>>> feature-branch
